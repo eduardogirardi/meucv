d5bac7c9763ecfef9a91041022b6cbab07efbd0c
eduardogirardi <eduardogirardi10@gmail.com>
2025-11-11 16:05:45 -0300

    update cv data source, comment out install lines, and add new CV-related files

--- code.R
+++ code.R
@@ -1,11 +1,9 @@
-install.packages("devtools")
-devtools::install_github("nstrayer/datadrivencv")
+# install.packages("devtools")
+# devtools::install_github("nstrayer/datadrivencv")
 
 
 datadrivencv::use_datadriven_cv(
     full_name = "Eduardo Girardi",
-    data_location = "https://docs.google.com/spreadsheets/d/1PIBINfzA5DJkLR-xzURLHwJA5Gce_JwTWTLWwv69S1Q/edit?usp=sharing",
-    pdf_location = "https://github.com/eduardogirardi/meucv/tree/main/docs/cv.pdf",
-    html_location = "https://github.com/eduardogirardi/meucv/tree/main/docs/",
-    source_location = "https://github.com/eduardogirardi/meucv/tree/main/"
+    data_location = "https://docs.google.com/spreadsheets/d/1IDKm2jIqQKrlsQ7omx2sGlZ8qCaTS8fJRe1dyG0Fnm0/edit?usp=sharing",
+    pdf_location = "https://github.com/eduardogirardi/meucv/tree/main/docs/cv.pdf"
 )
