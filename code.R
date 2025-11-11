b97f4cce47c0d6d67f82f7e5da1f756db66cc2c8
eduardogirardi <eduardogirardi10@gmail.com>
2025-11-11 11:29:43 -0300

    1 commit

--- code.R
+++ code.R
@@ -1,6 +1,7 @@
 install.packages("devtools")
 devtools::install_github("nstrayer/datadrivencv")
 
+
 datadrivencv::use_datadriven_cv(
     full_name = "Eduardo Girardi",
     data_location = "https://docs.google.com/spreadsheets/d/1bDEMbkHR6nZ_axMMkiA-lTa_MZfX3ufqqb07qB1ySuY/edit?usp=sharing",
