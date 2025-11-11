60b512050cbd8b72e95eb6d337a5fe2af5d1394a
eduardogirardi <eduardogirardi10@gmail.com>
2025-11-11 16:22:18 -0300

    Add source_location parameter to use_datadriven_cv function call

--- code.R
+++ code.R
@@ -5,5 +5,7 @@
 datadrivencv::use_datadriven_cv(
     full_name = "Eduardo Girardi",
     data_location = "https://docs.google.com/spreadsheets/d/1IDKm2jIqQKrlsQ7omx2sGlZ8qCaTS8fJRe1dyG0Fnm0/edit?usp=sharing",
-    pdf_location = "https://github.com/eduardogirardi/meucv/tree/main/docs/cv.pdf"
+    pdf_location = "https://github.com/eduardogirardi/meucv/tree/main/docs/cv.pdf",
+    source_location ="https://github.com/eduardogirardi/meucv/"
 )
+
