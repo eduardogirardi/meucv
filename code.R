8235885601e5799591069a5e9043e8bc51316a1c
eduardogirardi <eduardogirardi10@gmail.com>
2025-11-11 11:51:54 -0300

    Remove d.R file from pages directory

--- code.R
+++ code.R
@@ -5,7 +5,7 @@ devtools::install_github("nstrayer/datadrivencv")
 datadrivencv::use_datadriven_cv(
     full_name = "Eduardo Girardi",
     data_location = "https://docs.google.com/spreadsheets/d/1Se1mO4PQl7g8vzcBjhVnnaC8RtIkNZKC7DNbTbjpRkk/edit?usp=sharing",
-    pdf_location = "https://github.com/nstrayer/cv/raw/master/strayer_cv.pdf",
+    pdf_location = "https://github.com/eduardogirardi/meucv/tree/main/docs/cv.pdf",
     html_location = "pages/",
     source_location
 )
